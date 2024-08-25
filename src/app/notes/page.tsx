7010c2ed34a2c8ffff9d8431c772ea61d9bdda2c
ruin9999 <b14ktoss@gmail.com>
2024-08-25 13:47:27 +0800

    Completed rough notes section layout

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,70 +1,91 @@
-"use client"
+"use client";
 
 import { Input } from "@/components/ui/input";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import { ArrowPathIcon } from "@heroicons/react/24/solid";
 import { mockTags } from "@/data/mockTags";
-import { useState, useEffect, Children } from "react"
+import { useState, useEffect } from "react";
+import { ArrowUpTrayIcon } from "@heroicons/react/24/solid";
 
 export default function Page() {
-   const [ tagsFilter, setTagsFilter ] = useState("");
-   const [ selectedTagsFilter, setSelectedTagsFilter ] = useState<string[]>([])
+  const [tagsFilter, setTagsFilter] = useState("");
+  const [selectedTagsFilter, setSelectedTagsFilter] = useState<string[]>([]);
 
-   useEffect(() => {
-      console.log(tagsFilter)
-   }, [tagsFilter])
+  return (
+    <div className="min-h-[calc(100vh-61px)] px-[5%] grid grid-cols-12">
+      {/* Tags Section */}
+      <section className="col-span-4 border-r border-gray-100 pt-6 px-4">
+        <div className="flex gap-2">
+          <Input
+            className="bg-white"
+            placeholder="Filter tags"
+            value={tagsFilter}
+            onChange={(e) => setTagsFilter(e.target.value)}
+          />
+          <Button
+            variant="destructive"
+            onClick={() => setSelectedTagsFilter([])}
+          >
+            Reset
+          </Button>
+        </div>
 
-   useEffect(() => {
-      console.log(selectedTagsFilter);
-   }, [selectedTagsFilter])
+        <div className="flex flex-wrap gap-2 mt-4">
+          {mockTags.map((item) => {
+            if (!item.toLowerCase().includes(tagsFilter.toLowerCase()))
+              return null;
+            return (
+              <BadgeButton
+                key={item}
+                title={item}
+                isToggled={selectedTagsFilter.includes(item)}
+                onClick={() => {
+                  if (selectedTagsFilter.includes(item)) {
+                    setSelectedTagsFilter(
+                      selectedTagsFilter.filter((tag) => tag !== item)
+                    );
+                  } else {
+                    setSelectedTagsFilter([...selectedTagsFilter, item]);
+                  }
+                }}
+              />
+            );
+          })}
+        </div>
+      </section>
 
-   return(
-      <div className="min-h-[calc(100vh-61px)] px-[5%] grid grid-cols-12">
-
-         {/* Tags Section */}
-         <section className="col-span-4 border-r border-gray-100 pt-6 px-4">
-            <div className="flex gap-2">
-               <Input className="bg-white" placeholder="Filter tags" value={tagsFilter} onChange={(e) => setTagsFilter(e.target.value)} />
-               <Button variant="destructive" onClick={() => setSelectedTagsFilter([])}>
-                  {/* <ArrowPathIcon className="size-4" /> */}
-                  Reset
-               </Button>  
-            </div>
-            <div className="flex flex-wrap gap-2 mt-4">
-               {
-                  mockTags.map(item => {
-                     if(!item.toLowerCase().includes(tagsFilter.toLowerCase())) return null;
-                     return <BadgeButton key={item} title={item} isToggled={selectedTagsFilter.includes(item)} onClick={() => {
-                        if (selectedTagsFilter.includes(item)) {
-                           setSelectedTagsFilter(selectedTagsFilter.filter(tag => tag !== item));
-                        } else {
-                           setSelectedTagsFilter([...selectedTagsFilter, item]);
-                        }
-                     }} />
-                  })
-               }
-            </div>
-         </section>
-
-         {/* Notes Section */}
-         <section className="col-span-8 pt-6 px-4">
-            <div className="flex items-center text-lg gap-2">
-               <h1>Notes</h1>
-               <h2 className="text-slate-400">197</h2>
-               <Input placeholder="Filter by name" />
-            </div>
-         </section>
-      </div>
-   )
+      {/* Notes Section */}
+      <section className="col-span-8 pt-6 px-4">
+        <div className="flex items-center text-lg gap-2">
+          <h1>Notes</h1>
+          <h2 className="text-slate-400">197</h2>
+          <Input placeholder="Filter by name" />
+          <Button variant="secondary" className="shadow-sm">
+            <ArrowUpTrayIcon className="size-5" />
+          </Button>
+        </div>  
+      
+      </section>
+    </div>
+  );
 }
 
-function BadgeButton({ title, isToggled, onClick } : { 
-   title: string;
-   isToggled : boolean;
-   onClick: () => void;
+function BadgeButton({
+  title,
+  isToggled,
+  onClick,
+}: {
+  title: string;
+  isToggled: boolean;
+  onClick: () => void;
 }) {
-   return(
-      <Badge onClick={onClick} variant="outline" className={`hover:cursor-pointer hover:bg-slate-100 transition ${isToggled ? "bg-slate-100" : "bg-white"}`}>{ title }</Badge>
-   )
+  return (
+    <Badge
+      onClick={onClick}
+      variant="outline"
+      className={`hover:cursor-pointer hover:bg-slate-100 transition ${isToggled ? "bg-slate-100" : "bg-white"}`}
+    >
+      {title}
+    </Badge>
+  );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,14 @@
 "use client"
 
 import Image from "next/image";
+import { use, useState } from "react";
+import { useRouter } from "next/navigation";
 import { useUser } from "@clerk/nextjs";
-import { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Bars3Icon } from "@heroicons/react/24/solid";
 import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "@/components/ui/sheet";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 interface NavLinkProps {
   title: string | React.ReactNode;
@@ -23,17 +25,25 @@ const navLinks: NavLinkProps[] = [
 
 export default function Navbar() {
   const user = useUser(); 
+  const router = useRouter();
   const [ isNavOpen, setIsNavOpen ] = useState(false);
 
   return(
     <div className="flex justify-center border-b border-gray-100">
-      <div className="h-[60px] px-[5%] flex gap-2 items-center w-screen">
+      <div className="h-[60px] px-[5%] flex gap-4 items-center w-screen">
 
         <Image src="/Logo.png" alt="Noter logo" height={60} width={90} />
         <Input placeholder="Search notes, topics, users ...." />
 
-        <Button className="hidden lg:block" variant="ghost">Sign Up</Button>
-        <Button className="hidden lg:block">Sign In</Button>
+        <SignedIn>
+          <div className="hidden lg:block">
+            <UserButton />
+          </div>
+        </SignedIn>
+        <SignedOut>
+          <Button className="hidden lg:block" variant="ghost" onClick={() => router.push("/sign-up")}>Sign Up</Button>
+          <Button className="hidden lg:block" onClick={() => router.push("/sign-in")}>Sign In</Button>
+        </SignedOut>
 
         {/* Mobile UI */}
         <Sheet open={isNavOpen} onOpenChange={setIsNavOpen}>
