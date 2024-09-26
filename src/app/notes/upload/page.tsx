41dd6eb35e3b5d3cb507751f3ba72d75e4473f87
ruin9999 <b14ktoss@gmail.com>
2024-09-27 04:27:42 +0800

    Completed upload page

--- src/app/notes/upload/page.tsx
+++ src/app/notes/upload/page.tsx
@@ -1,85 +1,148 @@
-"use client";
+"use client"
 
-import { Label } from "@/components/ui/label";
+import { Loader2 } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { zodResolver } from "@hookform/resolvers/zod";
 import Tiptap from "@/components/common/editor/tiptap";
 import { TagSelector } from "@/components/common/tag-selector";
-import PageContainer from "@/components/layout/page-container";
-// import { QuestionMarkCircleIcon } from "@heroicons/react/24/outline";
-// import EditorShortcutsDialog from "@/components/common/editor-shortcuts-dialog";
-import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
 
 import { z } from "zod";
+import { useState } from "react";
 import { api } from "@convex/api";
 import DOMPurify from "dompurify";
 import { useUser } from "@clerk/nextjs";
-import { HTMLContent } from "@tiptap/core";
 import { useMutation } from "convex/react";
-import { useState, useEffect } from "react";
+import { HTMLContent } from "@tiptap/core";
 import { useRouter } from "next/navigation";
-import { stripHtmlTags } from "@/lib/utils";
-import { generateReactHelpers } from "@uploadthing/react";
-import { OurFileRouter } from "@/app/api/uploadthing/core";
-import CustomTooltip from "@/components/common/custom-tooltip";
 
-const { useUploadThing } = generateReactHelpers<OurFileRouter>();
 const formSchema = z.object({
-  title: z.string(),
+  title: z.string().min(1, "Title is required"),
   description: z.string(),
   tags: z.array(z.string()),
-})
+});
 
 export default function Page() {
-  const [ isEditorEmpty, setIsEditorEmpty ] = useState(true);
-  const [ editorTitle, setEditorTitle ] = useState<HTMLContent>("");
-  const [ editorContent, setEditorContent ] = useState<HTMLContent>("");
-  const [ selectedTags, setSelectedTags ] = useState<string[]>([]);
-
+  const [editorContent, setEditorContent] = useState<HTMLContent>("");
+  const [isLoading, setIsLoading] = useState(false); 
+  
   const router = useRouter();
   const { user } = useUser();
   const createNewNote = useMutation(api.notes.put.create);
 
-  function handleSubmit() {
-    if(editorTitle === "") return;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+      tags: [],
+    },
+  });
+
+  function handleSubmit(values: z.infer<typeof formSchema>) {
+    setIsLoading(true);
 
     const cleanHTML = DOMPurify.sanitize(editorContent);
     createNewNote({
-      description: "", 
-      html: cleanHTML, //TODO : remove title first then store as html.
+      description: values.description,
+      html: cleanHTML,
       fileUrl: "",
       fileId: "",
       stars: 0,
       downloads: 0,
-      tags: selectedTags,
-      title: editorTitle,
+      tags: values.tags,
+      title: values.title,
       userId: user?.id,
     })
-    .then(id => router.push(`/notes/view?id={id}`))
-    .catch(e => console.log(e));
+    .then(id => router.push(`/notes/view?id=${id}`))
+    .catch(e => {
+      console.log(e);
+      setIsLoading(false);
+    });
   }
 
-  useEffect(() => {
-    setIsEditorEmpty(editorTitle === "");
-  }, [editorTitle])
-
-  return (
-    <PageContainer className="relative flex-1 flex w-full">
-        <Tiptap 
+  return(
+    <ResizablePanelGroup
+      direction="horizontal"
+      className="flex-1"
+    >
+      <ResizablePanel defaultSize={70}>
+        <Tiptap
           initialValue=""
-          onTitleChange={setEditorTitle}
-          onContentChange={setEditorContent} 
+          className="p-4 no-scrollbar"
+          onChange={setEditorContent}
           editable
         />
-        <Dialog>
-          <DialogContent>
-            <DialogHeader>
-              Publishing to : <span className="font-bold">{user?.username || user?.fullName || ""}</span>
-            </DialogHeader>
-            <Label>Add or change topics so readers know what your note is about</Label>
-            <TagSelector value={selectedTags} onChange={setSelectedTags} />
-          </DialogContent>
-        </Dialog>
-      {/* <EditorShortcutsDialog trigger={<QuestionMarkCircleIcon className="size-10 stroke-1 fixed bottom-5 right-5" />} /> */}
-    </PageContainer>
-  );
+      </ResizablePanel>
+
+      <ResizableHandle withHandle />
+
+      <ResizablePanel defaultSize={30} maxSize={30}>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(handleSubmit)} className="p-4 gap-4 flex flex-col h-full">
+          <h1 className="text-3xl font-bold">Details</h1>
+          <FormField
+            control={form.control}
+            name="title"
+            render={({field}) => (
+              <FormItem>
+                <FormLabel>Title</FormLabel>
+                <FormControl>
+                  <Input placeholder="Write a title ..." {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="title"
+            render={({field}) => (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <Textarea 
+                    placeholder="Briefly describe your notes ..."
+                    className="max-h-[200px]"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="tags"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Tags</FormLabel>
+                <FormControl>
+                  <TagSelector value={field.value} onChange={field.onChange} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button
+            type="submit"
+            className="mt-auto"
+            disabled={isLoading} // Disable button when loading
+          >
+            {isLoading ? (
+              <>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                Uploading...
+              </>
+            ) : (
+              'Upload'
+            )}
+          </Button>
+        </form>
+      </Form>
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  )
 }
