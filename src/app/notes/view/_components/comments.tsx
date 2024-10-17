d064df7dd05f91781957bf4262989300b9e1f901
ruin9999 <b14ktoss@gmail.com>
2024-10-18 02:52:34 +0800

    Fixed issue causing rapid api calling. Added UI and comments scrolling.

--- src/app/notes/view/_components/comments.tsx
+++ src/app/notes/view/_components/comments.tsx
@@ -1,91 +1,136 @@
 "use client"
 
-import CommentBubble from "./comment-bubble";
+import { SendHorizonal } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
-import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetFooter, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import { Sheet, SheetContent, SheetHeader, SheetTitle } from "@/components/ui/sheet";
 
-import { boolean, z } from "zod";
+import axios from "axios";
+import { z } from "zod";
 import { api } from "@convex/api";
+import { Doc } from "@convex/dataModel";
 import { useUser } from "@clerk/nextjs";
-import { useForm } from "react-hook-form"
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import { useQuery, useMutation } from "convex/react"
+import { useForm } from "react-hook-form";
+import { User } from "@clerk/nextjs/server";
+import { formatDistanceToNow } from "date-fns";
+import { useEffect, useState, useRef } from "react";
+import { useQuery, useMutation } from "convex/react";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 interface CommentsProps {
-  fileId: string;
-  open?: boolean;
+  fileId : string;
+  open ?: boolean;
   onOpenChange?: (state: boolean) => void;
 }
 
 const formSchema = z.object({
-  comment: z.string().max(200, { message : "Maximum of 200 characters allowed."})
+  comment: z.string().max(200, { message: "Maximum of 200 characters allowed. "})
 })
 
-export default function Comments({ fileId, open, onOpenChange } : CommentsProps) {
-
+export default function Comments({ fileId, open, onOpenChange} : CommentsProps) {
   const user = useUser();
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-  })
+  const commentsRef = useRef<HTMLDivElement | null>(null);
+  const form = useForm<z.infer<typeof formSchema>>({ 
+    resolver: zodResolver(formSchema), 
+    mode: "onChange", 
+    defaultValues: {
+      comment: "",
+    }
+  });
 
+  const newComment = useMutation(api.comments.put.create)
+  const allComments = useQuery(api.comments.get.list, { fileId: fileId })
 
-  const newComment = useMutation(api.comments.put.create);
-  const allComments = useQuery(api.comments.get.list, { fileId: fileId });
-
-  const [localComments, setLocalComments] = useState(allComments);
-
-  useEffect(() => {
-    setLocalComments(allComments)
-  }, [allComments])
+  function handleKeyDown(event: React.KeyboardEvent<HTMLTextAreaElement>) {
+    if(event.key !== "Enter") return;
+    form.handleSubmit(handleSubmit)();
+    event.preventDefault();
+  }
 
   function handleSubmit(values: z.infer<typeof formSchema>) {
-    if(!user.isLoaded || !user.isSignedIn) return; //Ensure that we have a valid user first before we upload.
-
+    if(!user.isLoaded || !user.isSignedIn) return;
+    
     newComment({
       fileId: fileId,
       userId: user.user.id,
       content: values.comment,
-    })  
-  }
+    }).then(() => form.reset())
+  } 
 
-  return(
-    <Sheet open={open} onOpenChange={onOpenChange}>
-      <SheetContent>
-        <SheetHeader>
-          <SheetTitle>Comments</SheetTitle>
-        </SheetHeader>
+	useEffect(() => {
+		if (commentsRef.current) {
+			commentsRef.current.scrollTop = commentsRef.current.scrollHeight;
+		}
+	}, [allComments])
 
-        <div className="space-y-2">
-          {localComments?.map((comment) => {
-            return <div key={comment._id}>
-              {CommentBubble({userId: comment.userId, content: comment.content})}
-              </div>
-          })}
-        </div>
+  return(<Sheet open={open} onOpenChange={onOpenChange}>
+    <SheetContent className="flex flex-col gap-1 p-4">
+      <SheetHeader>
+        <SheetTitle className="text-left">Comments</SheetTitle>
+      </SheetHeader>
 
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-2 my-2">
-            <FormField
-              control={form.control}
-              name="comment"
-              render={({field}) => (
-                <FormItem>
-                  <FormControl>
-                    <Textarea placeholder="What are your thoughts?" className="max-h-[200px]" {...field} />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
+      <div className="flex-grow overflow-auto no-scrollbar space-y-3" ref={commentsRef}>
+        {allComments?.map((comment) => <CommentBubble key={comment._id} comment={comment} />)}
+      </div>
 
-            <Button type="submit" variant="default">Comment</Button>
-          </form>
-        </Form>
-      </SheetContent>
-    </Sheet>
-  );
+      <Form {...form}>
+        <form className="my-2 border rounded-md">
+          <FormField
+            control={form.control}
+            name="comment"
+            render={({field}) => (
+              <FormItem>
+                <FormControl>
+                  <div className="flex gap-2 pr-2 items-center">
+                    <Textarea
+                      placeholder="What are your thoughts?"
+                      className="max-h-[100px] resize-none border-none outline-none shadow-none no-scrollbar"
+                      onKeyDown={handleKeyDown}
+                      {...field}
+                    />
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </SheetContent>
+  </Sheet>)
+}
+
+
+
+function CommentBubble({ comment } : { comment : Doc<"comments">}) {
+
+  const [user, setUser] = useState<User>();
+  
+  useEffect(() => {
+    async function GetUserInformation() {
+      const { data } = await axios.get("/api/user", {
+        params: { id: comment.userId }
+      })
+
+      setUser(data.user)
+    }
+    GetUserInformation();
+  }, [comment])
+  
+  return(<div className="flex items-start space-x-3 py-2 px-3">
+    <Avatar className="size-10 mt-1">
+      <AvatarImage src={user?.imageUrl} />
+      <AvatarFallback>{user?.username?.slice(0,2).toUpperCase()}</AvatarFallback>
+    </Avatar>
+    <div className="flex-grow min-w-0">
+      <div className="flex items-center space-x-2">
+        <h3 className="text-sm font-medium text-foreground truncate">{user?.username || user?.firstName || user?.lastName}</h3>
+        <span className="text-xs text-muted-foreground">•</span>
+        <span className="text-xs text-muted-foreground whitespace-nowrap">{formatDistanceToNow(new Date(comment._creationTime), { addSuffix: true })}</span>
+      </div>
+      <p className="mt-0.5 text-sm text-foreground break-words">{comment.content}</p>
+    </div>
+  </div>)
 }
