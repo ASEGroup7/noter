d7e25db8fe8b844508d44cb2d1e3767639ad9f02
ruin9999 <b14ktoss@gmail.com>
2024-10-21 21:44:14 +0800

    Re-committed convex generated types

---
+++ convex/_generated/api.d.ts
@@ -0,0 +1,50 @@
+/* prettier-ignore-start */
+
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+import type * as comments_get from "../comments/get.js";
+import type * as comments_put from "../comments/put.js";
+import type * as notes_delete from "../notes/delete.js";
+import type * as notes_get from "../notes/get.js";
+import type * as notes_put from "../notes/put.js";
+import type * as tags_get from "../tags/get.js";
+
+/**
+ * A utility for referencing Convex functions in your app's API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
+declare const fullApi: ApiFromModules<{
+  "comments/get": typeof comments_get;
+  "comments/put": typeof comments_put;
+  "notes/delete": typeof notes_delete;
+  "notes/get": typeof notes_get;
+  "notes/put": typeof notes_put;
+  "tags/get": typeof tags_get;
+}>;
+export declare const api: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "public">
+>;
+export declare const internal: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "internal">
+>;
+
+/* prettier-ignore-end */
