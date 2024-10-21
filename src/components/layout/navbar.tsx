dbdce33b7ee9c4ab99d5e6a26b953a2a55e57787
ruin9999 <b14ktoss@gmail.com>
2024-10-21 20:40:57 +0800

    Removed the ability to access dropdown if not signed in

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -34,95 +34,99 @@ export function Navbar() {
     <nav className="flex items-center justify-between p-4 border-b border-gray-100">
       <div className="flex items-center space-x-4">
         <div className="relative w-8 h-8 sm:w-10 sm:h-10">
-          <Image
-            src="/Logo.png"
-            alt="Noter logo"
-            layout="fill"
-            objectFit="contain"
-            priority
-          />
+          <Link href="/notes">
+            <Image
+              src="/Logo.png"
+              alt="Noter logo"
+              layout="fill"
+              objectFit="contain"
+              priority
+            />
+          </Link>
         </div>
         <CmdkSearchBar className="hidden sm:flex" onActivate={() => setIsDialogOpen(true)} />
       </div>
 
-      <div className="flex items-center space-x-4">
-        <DropdownMenu open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
-          <DropdownMenuTrigger asChild>
-            <Avatar className="hidden sm:block">
-              <AvatarImage src={user.user?.imageUrl} />
-              <AvatarFallback>{user.user?.username?.[0]}</AvatarFallback>
-            </Avatar>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-[200px] translate-x-[-10px]">
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <DropdownMenuItemLink href={`/profile/${user.user?.id}`} onClick={() => setIsDropdownOpen(false)}>
-                  <UserIcon className="size-4" />
-                  <span>Profile</span>
-                </DropdownMenuItemLink>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <DropdownMenuItemLink href="/library" onClick={() => setIsDropdownOpen(false)}>
-                  <BookmarkIcon className="size-4" />
-                  <span>My notes</span>
-                </DropdownMenuItemLink>
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
+      <SignedIn>
+        <div className="flex items-center space-x-4">
+          <DropdownMenu open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
+            <DropdownMenuTrigger asChild>
+              <Avatar className="hidden sm:block">
+                <AvatarImage src={user.user?.imageUrl} />
+                <AvatarFallback>{user.user?.username?.[0]}</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-[200px] translate-x-[-10px]">
+              <DropdownMenuGroup>
+                <DropdownMenuItem>
+                  <DropdownMenuItemLink href={`/profile/${user.user?.id}`} onClick={() => setIsDropdownOpen(false)}>
+                    <UserIcon className="size-4" />
+                    <span>Profile</span>
+                  </DropdownMenuItemLink>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <DropdownMenuItemLink href="/library" onClick={() => setIsDropdownOpen(false)}>
+                    <BookmarkIcon className="size-4" />
+                    <span>My notes</span>
+                  </DropdownMenuItemLink>
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
 
-            {/* Admin page */}
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <DropdownMenuItemLink href="/admin" onClick={() => setIsDropdownOpen(false)}>
-                  <Cog className="size-4" />
-                  <span>Admin dashboard</span>
-                </DropdownMenuItemLink>
+              {/* Admin page */}
+              <DropdownMenuGroup>
+                <DropdownMenuItem>
+                  <DropdownMenuItemLink href="/admin" onClick={() => setIsDropdownOpen(false)}>
+                    <Cog className="size-4" />
+                    <span>Admin dashboard</span>
+                  </DropdownMenuItemLink>
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+
+              <DropdownMenuItem onClick={() => signOut()} className="flex items-center gap-2 w-full text-red-500">
+                <LogOut className="size-4" />
+                <span>Sign out</span>
               </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
+            </DropdownMenuContent>
+          </DropdownMenu>
 
-            <DropdownMenuItem onClick={() => signOut()} className="flex items-center gap-2 w-full text-red-500">
-              <LogOut className="size-4" />
-              <span>Sign out</span>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-
-        {/* Mobile UI */}
-        <Sheet>
-          <SheetTrigger asChild>
-            <Menu className="size-5 sm:hidden" />
-          </SheetTrigger>
-          <SheetContent>
-            <SheetHeader>
-              <SheetTitle className="text-left text-3xl">Menu</SheetTitle>
-            </SheetHeader>
-            <div className="h-full flex flex-col py-3">
-              <div className="flex-1">
-                <div
-                  onClick={() => setIsDialogOpen(true)}
-                  className={cn(
-                    inputStyles,
-                    "flex items-center w-full gap-2 text-muted-foreground hover:cursor-text mr-auto rounded-full",
-                  )}>
-                  <Search className="size-4" />
-                  <span>Search notes</span>
+          {/* Mobile UI */}
+          <Sheet>
+            <SheetTrigger asChild>
+              <Menu className="size-5 sm:hidden" />
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle className="text-left text-3xl">Menu</SheetTitle>
+              </SheetHeader>
+              <div className="h-full flex flex-col py-3">
+                <div className="flex-1">
+                  <div
+                    onClick={() => setIsDialogOpen(true)}
+                    className={cn(
+                      inputStyles,
+                      "flex items-center w-full gap-2 text-muted-foreground hover:cursor-text mr-auto rounded-full",
+                    )}>
+                    <Search className="size-4" />
+                    <span>Search notes</span>
+                  </div>
+                </div>
+                <div className="flex items-start space-x-3 py-4">
+                  <Avatar className="size-9 mt-1">
+                    <AvatarImage src={user.user?.imageUrl} />
+                    <AvatarFallback>{user.user?.username?.[0] || user.user?.fullName?.[0] || ""}</AvatarFallback>
+                  </Avatar>
+                  <div className="flex-grow min-w-0">
+                    <h3 className="text-sm font-medium text-foreground truncate">{user.user?.username || user.user?.firstName || user.user?.lastName}</h3>
+                    <p className="text-sm font-bold">{user.user?.primaryEmailAddress?.emailAddress || ''}</p>
+                  </div>
                 </div>
               </div>
-              <div className="flex items-start space-x-3 py-4">
-                <Avatar className="size-9 mt-1">
-                  <AvatarImage src={user.user?.imageUrl} />
-                  <AvatarFallback>{user.user?.username?.[0] || user.user?.fullName?.[0] || ""}</AvatarFallback>
-                </Avatar>
-                <div className="flex-grow min-w-0">
-                  <h3 className="text-sm font-medium text-foreground truncate">{user.user?.username || user.user?.firstName || user.user?.lastName}</h3>
-                  <p className="text-sm font-bold">{user.user?.primaryEmailAddress?.emailAddress || ''}</p>
-                </div>
-              </div>
-            </div>
-          </SheetContent>
-        </Sheet>
-      </div>
+            </SheetContent>
+          </Sheet>
+        </div>
+      </SignedIn>
 
       <CommandDialog open={isDialogOpen} onOpenChange={setIsDialogOpen} shouldFilter={false}>
         <CommandInput
