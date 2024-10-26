22b1877361cb5473184f190e8a5e9a2fc1074e8d
jesicatjan <jesicatjan11@gmail.com>
2024-10-27 01:10:11 +0800

    comment workflow and added video function

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,5 +5,9 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    video: true, // Enable video recording
+    videoCompression: 32, // Set the compression level (0 to 51)
+    videosFolder: 'cypress/videos', // Specify the folder to save videos
+    // Additional configuration options can be added here
   },
 });
